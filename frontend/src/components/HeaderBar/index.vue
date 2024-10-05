d7940ce2e29c169da6299b27bfc583a33088da91
lecaohao2101 <lecaohao2101@gmail.com>
2024-10-05 20:36:03 +0700

    reponsive header

--- frontend/src/components/HeaderBar/index.vue
+++ frontend/src/components/HeaderBar/index.vue
@@ -3,12 +3,20 @@
 
       .header-color
         v-list-item.pa-3.center
-          v-img(
+          v-img.hidden-md-and-down(
           src="https://simicart.com/wp-content/uploads/eCommerce-logo.jpg"
           max-width="40"
           )
           v-spacer
-          v-text-field(
+          v-text-field.hidden-md-and-down(
+            label="Search"
+            placeholder="Product"
+            solo
+            hide-details
+            background-color="white"
+            prepend-inner-icon="mdi-home-search-outline"
+            )
+          v-text-field.hidden-md-and-up(
             label="Search"
             placeholder="Product"
             solo
@@ -18,25 +26,35 @@
             )
           v-spacer
           div.pa-2
-            v-icon(color="white" ) mdi-home
-            span.white--text Home
+            v-icon(color="white") mdi-home
+            span.white--text.hidden-md-and-down Home
           div.pa-2
-            v-icon(color="white" ) mdi-account
-            span.white--text Account
+            v-icon(color="white") mdi-account
+            span.white--text.hidden-md-and-down Account
           div.pl-10
             v-btn(icon)
-              v-icon(color="white" ) mdi-cart-outline
+              v-icon(color="white") mdi-cart-outline
+          div.hidden-md-and-up
+            v-icon(color="white" @click="isShowCategoryInMobile = !isShowCategoryInMobile") mdi-menu
         hr
-        .center
+        .pc.center.hidden-md-and-down
           ul
             li.pa-5.white--text(v-for="category in categoriesTop") {{category}}
-
+        .center.hidden-md-and-up
+          v-expand-transition
+            v-card(
+              color="transparent"
+              v-show="isShowCategoryInMobile"
+            )
+              ul
+                li.pa-5.white--text(v-for="category in categoriesTop") {{category}}
 </template>
 
 <script lang="ts">
 const HeaderBar ={
   data() {
     return {
+      isShowCategoryInMobile: false,
       categoriesTop:[
         "Home",
         "Featured",
@@ -46,7 +64,8 @@ const HeaderBar ={
         "Products by Brand"
       ]
     }
-  }
+  },
+
 }
 
 export default HeaderBar
@@ -55,17 +74,24 @@ export default HeaderBar
 <style scoped lang="sass">
 .header-color
   background-color: #f57e2e
+
 .center
   margin: 0 auto
   text-align: center
+
 ul
   justify-content: center
+  padding: 0
+
 ul li
   list-style: none
-  display: inline-block
   margin: 0 3px
+
+.pc ul li
+    display: inline-block
+
 ul li:hover
-  background-color: #faabcd
+  background-color: #e19669
   cursor: pointer
 
 
