d94287cbddf869578df65a909a5fe2ea912d693d
lecaohao2101 <lecaohao2101@gmail.com>
2024-10-06 00:55:37 +0700

    categories, banner, ads

---
+++ frontend/src/components/Categories/index.vue
@@ -0,0 +1,59 @@
+<template lang="pug">
+  .border.width-card
+    //v-card.mx-auto(max-width="300" title="")
+    v-list(dense="")
+      v-subheader SP Noi Bat
+      v-list-item-group(v-model="selectedItem" color="#e19669")
+        v-list-item(v-for='(item, i) in items' :key='i')
+          v-img(:src="item.icon" max-width="30px")
+          span.pl-4 {{item.text}}
+</template>
+
+
+<script lang="ts">
+const Categories = {
+  data() {
+    return {
+      selectedItem: 1,
+      items: [
+        { text: 'Airpods', icon: '' },
+        { text: 'Apple Watch', icon: 'mdi-account' },
+        { text: 'Book', icon: 'https://www.novelkicks.co.uk/wp-content/uploads/2013/05/leather-book-preview.png' },
+        { text: 'Cosmetics', icon: 'https://png.pngtree.com/png-vector/20240818/ourmid/pngtree-geset-of-luxury-beauty-cosmetic-makeup-bdifferent-png-image_13532167.png' },
+        { text: 'Clothes', icon: '' },
+        { text: 'Glasses', icon: '' },
+        { text: 'Gadgets', icon: 'mdi-account' },
+        { text: 'Handbags', icon: 'mdi-flag' },
+        { text: 'Ipad', icon: 'mdi-flag' },
+        { text: 'Kid\'s Fashion', icon: 'mdi-account' },
+        { text: 'Laptop', icon: '' },
+        { text: 'Men\'s Fashion', icon: 'mdi-account' },
+        { text: 'Sports', icon: 'mdi-flag' },
+        { text: 'PC', icon: 'mdi-clock' },
+        { text: 'Toys', icon: 'mdi-account' },
+        { text: 'Woman\'s Fashion', icon: 'mdi-account' },
+
+
+
+
+      ],
+    }
+  }
+}
+
+export default Categories
+</script>
+
+
+
+
+<style scoped lang="sass">
+
+.border
+  //border-radius: 15px
+  border: 2px solid
+  color: #f57e2e
+.width-card
+  max-width: 250px
+
+</style>
--- frontend/src/pages/Home/index.vue
+++ frontend/src/pages/Home/index.vue
@@ -1,18 +1,39 @@
 <template lang="pug">
   div
     header-bar
-    h1 day la home
+    .max-width.center
+      v-row.pt-10
+        v-col(cols="2")
+          categories
+        v-col(cols="8")
+          carousel
+        v-col(cols="2")
+          v-img(src="https://t3.ftcdn.net/jpg/04/65/46/52/360_F_465465254_1pN9MGrA831idD6zIBL7q8rnZZpUCQTy.jpg")
+      h1 day la home
+
 </template>
 
 <script lang="ts">
 import HeaderBar from "@/components/HeaderBar/index.vue";
+import Categories from "@/components/Categories/index.vue";
+import Carousel from "@/components/Carousel/index.vue";
 const Home ={
   components: {
-    HeaderBar
+    HeaderBar,
+    Categories,
+    Carousel
+
   }
 }
 
 export default Home
 </script>
 
-<style lang="scss"></style>
+<style scoped lang="sass">
+.max-width
+  max-width: 1500px
+.center
+  margin: 0 10px 0 10px
+  text-align: center
+
+</style>
